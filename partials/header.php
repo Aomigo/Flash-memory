2de0998d9bd298ba81f229b10fa6665dae5200d0
Aomigo <bingoash36@gmail.com>
2025-11-06 22:55:49 +0100

    added username in header

--- partials/header.php
+++ partials/header.php
@@ -1,20 +1,26 @@
-
 <header>
-        <!--the nav composed of 4 elements-->
-        <nav>
-            <div class="logo"> <img src="<?php echo RootUrl(); ?>assets/images/logo.png" alt="logo"><strong>Power of memory</strong></div>
-            <div class="burger">
-                <div class="line"></div>
-                <div class="line"></div>
-                <div class="line"></div>
+    <!--the nav composed of 4 elements-->
+    <nav>
+        <div class="logo"> <img src="<?php echo RootUrl(); ?>assets/images/logo.png" alt="logo">
+            <div class="logo-wrapper">
+                <strong>Power of memory</strong>
+                <?php if (isset($_SESSION['user'])) {
+                    echo '<p class="feint">' . $_SESSION['user']['pseudo'] . '</p>';
+                } ?>
             </div>
-            <ul class="nav">
-                <li>
-                    <a class="<?php active('index.php') ?>" href="<?php echo RootUrl(); ?>index.php">Home</a>
-                    <a class="<?php active('score.php') ?>" href="<?php echo RootUrl(); ?>games/memory/score.php">Score</a>
-                    <a class="<?php active('account.php') ?>" href="<?php echo RootUrl(); ?>account.php">My account</a>
-                    <a href="<?php echo RootUrl(); ?>contact.php"><button>Contact Us</button></a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+        </div>
+        <div class="burger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <ul class="nav">
+            <li>
+                <a class="<?php active('index.php') ?>" href="<?php echo RootUrl(); ?>index.php">Home</a>
+                <a class="<?php active('score.php') ?>" href="<?php echo RootUrl(); ?>games/memory/score.php">Score</a>
+                <a class="<?php active('account.php') ?>" href="<?php echo RootUrl(); ?>account.php">My account</a>
+                <a href="<?php echo RootUrl(); ?>contact.php"><button>Contact Us</button></a>
+            </li>
+        </ul>
+    </nav>
+</header>
